74122c4704219189d41b3b8c4b3fb91280ce1e26
metacalled <46867837+metaacalled@users.noreply.github.com>
2025-05-26 00:39:20 +0200

    Create discord.lua

---
+++ discord.lua
@@ -0,0 +1,13 @@
+local discord = {}
+
+local function discord.callWebhook(url, username, title, message)
+    local data = { ["username"] = username, ["content"] = message, ["embeds"] = { { ["title"] = title} } }
+    local headers = { ["Content-Type"] = "application/x-www-form-urlencoded" }
+    local request, message = http.post(url, data, headers)
+    if not request then
+        return false
+    end
+    return true
+end
+
+return discord
