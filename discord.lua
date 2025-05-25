004b927d1f4c4cb6bf4fca70029cb870795eaf59
metacalled <46867837+metaacalled@users.noreply.github.com>
2025-05-26 00:53:56 +0200

    Update discord.lua

--- discord.lua
+++ discord.lua
@@ -2,7 +2,7 @@ local discord = {}
 
 function discord.callWebhook(url, username, title, message)
     local data = { ["username"] = username, ["content"] = message, ["embeds"] = { { ["title"] = title} } }
-    local headers = { ["Content-Type"] = "application/x-www-form-urlencoded" }
+    local headers = { ["Content-Type"] = "application/json" }
     local request, message = http.post(url, data, headers)
     if not request then
         return false
